61e24b17d12457170173d4c2ca418824b3b16d0c
Benjamin Di Tella <benja.m.dite@gmail.com>
2024-05-29 20:08:35 -0300

    feat: CRUD de provedores completo

--- project/producto/urls.py
+++ project/producto/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('provedor/crear', views.provedor_crear, name='provedor_crear'), #solo deberia ser accesible por admin
     path('provedor/listar', views.provedor_listar, name='provedor_listar'),
-    #path('provedor/actualizar', views.provedor_actualizar, name='provedor_actualizar'), #solo deberia ser accesible por admin
+    path('provedor/detalle/<int:pk>', views.provedor_detalle, name='provedor_detalle'), #solo deberia ser accesible por admin
+    path('provedor/actualizar', views.provedor_actualizar, name='provedor_actualizar'), #solo deberia ser accesible por admin
+    path('provedor/eliminar', views.provedor_eliminar, name='provedor_eliminar'), #solo deberia ser accesible por admin
+
 ]
--- project/producto/views.py
+++ project/producto/views.py
@@ -21,5 +21,29 @@ def provedor_listar(request):
     contexto = {'Provedores': consulta}
     return render(request, 'producto/provedor_listar.html', contexto)
 
-def provedor_actualizar(request):
-    ...
+def provedor_detalle(request, pk:int):
+    consulta = Provedor.objects.get(id=pk)
+    context = {"provedor" : consulta}
+    return render(request, 'producto/provedor_detalle.html', context)
+
+
+
+def provedor_actualizar(request, pk:int):
+    consulta = Provedor.objects.get(id=pk)
+    if request.method == 'POST':
+        form = provedorForm(request.POST, instance=consulta)
+        if form.is_valid():
+            form.save()
+            return redirect('producto:provedor_listar')
+    else: #get
+        form = provedorForm(instance=consulta)
+    return render(request, 'producto/provedor_actualizar.html', {'form': form})
+
+
+def provedor_eliminar(request):
+    consulta = Provedor.objects.get(id=pk)
+    if request.method == 'POST':
+        consulta.delete
+        return redirect('producto:provedor_listar')
+    else:
+        return render(request, 'provedor_eliminar.html', {'Provedor':consulta})
