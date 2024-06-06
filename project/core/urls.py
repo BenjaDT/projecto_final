75c7d6e3bdd9a62651946d2462381fc6e35c066b
Benjamin Di Tella <benja.m.dite@gmail.com>
2024-06-06 19:50:52 -0300

    feat: register user

--- project/core/urls.py
+++ project/core/urls.py
@@ -7,5 +7,6 @@ app_name= 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/',views.LogoutView.as_view(template_name='core/logout.html'), name='logout')
+    path('logout/',views.LogoutView.as_view(template_name='core/logout.html'), name='logout'),
+    path('register/', ... , name='register')
 ] 
--- project/core/views.py
+++ project/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 from django.contrib.auth.views import LoginView, LogoutView
-from .forms import CustomAuthenticationForm
+from .forms import CustomAuthenticationForm ,CustomUserCreationForm
 
 # Create your views here.
 
@@ -10,5 +11,16 @@ def index(request):
 class CustomLoginView(LoginView):
     authentication_form = CustomAuthenticationForm
     template_name = 'core/login.html'
+
+# falta logout??
+
+def register(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = CustomUserCreationForm()
+    return render(request,'core/register', {'form' : form})
 
 
